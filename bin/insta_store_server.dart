47003666ec9ff82bd3c013a988980d23bb127321
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-03-16 17:05:59 +0530

    initial commit

---
+++ bin/insta_store_server.dart
@@ -0,0 +1,5 @@
+import 'package:insta_store_server/insta_store_server.dart' as insta_store_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${insta_store_server.calculate()}!');
+}
---
+++ test/insta_store_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:insta_store_server/insta_store_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
