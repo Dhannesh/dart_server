c8760395b07f93adb33c488b77484434e85cb39d
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-03-17 06:43:47 +0530

    product add feature completed

--- bin/insta_store_server.dart
+++ bin/insta_store_server.dart
@@ -1,5 +1,6 @@
-import 'package:insta_store_server/insta_store_server.dart' as insta_store_server;
+import 'package:insta_store_server/insta_store_server.dart'
+    as insta_store_server;
 
 void main(List<String> arguments) {
-  print('Hello world: ${insta_store_server.calculate()}!');
+  print('Hello world: ${insta_store_server.main()}!');
 }
